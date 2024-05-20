a411a8ac05b4521825a4da2e5bf09167a1632e17
Miny <944307891@qq.com>
2024-05-20 20:44:23 +0800

    feat: 更新返回结果

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -4,7 +4,7 @@ import { PRIVATE_KEY } from '@/config/secret'
 
 class AuthController {
   async login(ctx: Context) {
-    const { id, login_name } = ctx.user
+    const { id, login_name, role_id } = ctx.user
 
     const token = jwt.sign({ id, login_name }, PRIVATE_KEY, {
       expiresIn: 60 * 60 * 24,
@@ -13,15 +13,16 @@ class AuthController {
 
     ctx.body = {
       code: 200,
-      data: { id, name: login_name, token },
+      data: { id, name: login_name, role_id, token },
       msg: '登录成功',
     }
   }
 
   async success(ctx: Context) {
+    const user = ctx.user
     ctx.body = {
       code: 200,
-      data: null,
+      data: user,
       msg: '授权成功',
     }
   }
